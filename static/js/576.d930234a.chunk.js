6d60f3c30251d6516024191495499bbddf866653
Stribx <88462017+Stribx@users.noreply.github.com>
2022-06-16 12:19:52 +0200

    Updates

---
+++ static/js/576.d930234a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunknavi=self.webpackChunknavi||[]).push([[576],{576:function(i,n,t){t.r(n),t.d(n,{default:function(){return c}});var e=t(285),a=t(184);function c(){return(0,a.jsx)(e.motion.div,{className:"home",initial:{opacity:0},animate:{opacity:1},exit:{opacity:0},children:(0,a.jsx)("p",{children:"Agent"})})}}}]);
+//# sourceMappingURL=576.d930234a.chunk.js.map
